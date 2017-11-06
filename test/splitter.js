e04e1f7b805a2b8b85b8495a2ee771f2433e3fe5
Daniel Luca <daniel.luca@consensys.net>
2017-11-06 10:12:15 +0200

    Improve tests

--- test/splitter.js
+++ test/splitter.js
@@ -5,8 +5,9 @@ var BigNumber = require('bignumber.js');
 
 contract('Splitter', function (accounts) {
     const owner = accounts[0];
-    const partner1 = accounts[1];
-    const partner2 = accounts[2];
+    const partner_one = accounts[1];
+    const partner_two = accounts[2];
+    const payer = accounts[9];
 
     it('should not error', function (done) {
         assert.isTrue(true);
@@ -14,65 +15,55 @@ contract('Splitter', function (accounts) {
     });
 
     it('should save partner with correct weight', function () {
-        var instance;
-        return Splitter.deployed().then(function (inst) {
-            instance = inst;
-            return instance.partnerAdd(partner1, 100, { from: owner })
+        var splitter;
+        return Splitter.new({ from: owner }).then(function (instance) {
+            splitter = instance;
+            return splitter.partnerAdd(partner_one, 100, { from: owner })
         }).then(function (result) {
             assert.equal(1, result.receipt.status, 'Transaction adding partner not successful');
-            return instance.partnerExists(partner1);
+            return splitter.partnerExists(partner_one);
         }).then(function (result) {
             assert.isTrue(result, 'existsPartner() should return true added partner');
         }).then(function () {
-            return instance.partnerWeight(partner1);
+            return splitter.partnerWeight(partner_one);
         }).then(function (result) {
             assert.isTrue(result.equals(100), 'Weight should match the saved weight for a partner');
         })
     });
 
-    // TODO: test edge cases for adding / removing partners
-});
-
-contract('Splitter', function (accounts) {
     it('should split funds to registered partners', function () {
         var splitter;
-
-        var partner_one = "0x905efee6442b06dc78af204f7df0afbfa52cede6";
-        var partner_two = "0xd71e7b8ffe88568d88b67f28089483c62e23f798";
-        var payer = accounts[0];
 
         var sendValue = new BigNumber(web3.toWei(100, "wei"))
 
         var partner_one_starting_balance = web3.eth.getBalance(partner_one);
-        var partner_one_ending_balance;
         var partner_two_starting_balance = web3.eth.getBalance(partner_two);
-        var partner_two_ending_balance;
 
-        return Splitter.deployed().then(function (inst) {
-            splitter = inst;
-            splitter.partnerAdd(partner_one, 1);
+        return Splitter.new({ from: owner }).then(function (instance) {
+            splitter = instance;
+            splitter.partnerAdd(partner_one, 1, { from: owner });
         }).then(function () {
-            splitter.partnerAdd(partner_two, 1);
+            splitter.partnerAdd(partner_two, 1, { from: owner });
         }).then(function () {
             return splitter.partnerExists.call(partner_one);
         }).then(function (result) {
-            assert.isTrue(result);
-            return splitter.partnerExists.call(partner_one);
+            assert.isTrue(result, "Owner should be able to add partner");
+            return splitter.partnerExists.call(partner_two);
         }).then(function (result) {
             assert.isTrue(result);
         }).then(function () {
-            return splitter.split({ value: sendValue });
+            return splitter.split({ from: payer, value: sendValue });
         }).then(function (result) {
             assert.equal(
                 web3.eth.getBalance(partner_one).toNumber(),
                 partner_one_starting_balance.add(sendValue.div(2)).toNumber(),
-                "Partner one should increase its balance by 50 wei"
+                "Partner one should receive 50 wei"
             )
 
             assert.equal(
                 web3.eth.getBalance(partner_two).toNumber(),
                 partner_two_starting_balance.add(sendValue.div(2)).toNumber(),
-                "Partner two should increase its balance by 50 wei"
+                "Partner two should receive 50 wei"
             )
         });
     })
@@ -92,7 +83,7 @@ contract('Splitter', function (accounts) {
         }).then(function (result) {
             assert.equal(0, result.receipt.status, "Adding a partner by a non partner / non owner should fail");
             return splitter.partnerExists(partner_two);
-        }).then(function( result) {
+        }).then(function (result) {
             assert.isTrue(!result, "Should not have been able to add a partner");
         });
     });
