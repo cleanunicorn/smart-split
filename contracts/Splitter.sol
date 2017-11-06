8b4afe7dda332d28b41b90d9dcef06df23e1dd59
Daniel Luca <daniel.luca@consensys.net>
2017-11-06 09:41:09 +0200

    Allow only owner and partners to add

--- contracts/Splitter.sol
+++ contracts/Splitter.sol
@@ -2,21 +2,44 @@ pragma solidity ^0.4.15;
 
 contract Splitter {
     // state
+    address owner;
     address[] private partners;
     uint256[] partnersWeight;
+
+    // Constructor
+    function Splitter() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwnerOrPartner() {
+        bool allowed = false;
+
+        if (msg.sender == owner) {
+            allowed = true;
+        }
+
+        for (uint256 i = 0; i < partners.length; i++) {
+            if (msg.sender == partners[i]) {
+                allowed = true;
+            }
+        }
+
+        require(allowed);
+        _;
+    }
 
     event SplitValue(address receiver, uint256 weight, uint256 amount);
     event Sum(uint256 sum);
     event SplitValueReceived(uint256 amount);
 
-    function partnerAdd(address partner, uint256 weight) public {
-        require(partnersExists(partner) == false);
+    function partnerAdd(address partner, uint256 weight) onlyOwnerOrPartner public {
+        require(partnerExists(partner) == false);
 
         partners.push(partner);
         partnersWeight.push(weight);
     }
 
-    function partnersExists(address partner) constant public returns (bool) {
+    function partnerExists(address partner) constant public returns (bool) {
         for (uint256 i = 0; i < partners.length; i++) {
             if (partner == partners[i]) {
                 return true;
--- test/splitter.js
+++ test/splitter.js
@@ -3,28 +3,29 @@ console.log('Testing starting....');
 var Splitter = artifacts.require("./Splitter.sol");
 var BigNumber = require('bignumber.js');
 
-contract('Splitter', function(accounts){
-    const partner1 = web3.eth.accounts[0];
-    const partner2 = web3.eth.accounts[1];
+contract('Splitter', function (accounts) {
+    const owner = accounts[0];
+    const partner1 = accounts[1];
+    const partner2 = accounts[2];
 
-    it('Should not error', function(done) {
+    it('should not error', function (done) {
         assert.isTrue(true);
         done();
     });
 
-    it('Should save partner with correct weight', function() {
+    it('should save partner with correct weight', function () {
         var instance;
-        return Splitter.deployed().then(function(inst) {
+        return Splitter.deployed().then(function (inst) {
             instance = inst;
-            return instance.partnerAdd(partner1, 100)
-        }).then(function(result) {
+            return instance.partnerAdd(partner1, 100, { from: owner })
+        }).then(function (result) {
             assert.equal(1, result.receipt.status, 'Transaction adding partner not successful');
-            return instance.partnersExists(partner1);
-        }).then(function(result) {
+            return instance.partnerExists(partner1);
+        }).then(function (result) {
             assert.isTrue(result, 'existsPartner() should return true added partner');
-        }).then(function() {
+        }).then(function () {
             return instance.partnerWeight(partner1);
-        }).then(function(result) {
+        }).then(function (result) {
             assert.isTrue(result.equals(100), 'Weight should match the saved weight for a partner');
         })
     });
@@ -33,7 +34,7 @@ contract('Splitter', function(accounts){
 });
 
 contract('Splitter', function (accounts) {
-    it('Should split funds to registered partners', function () {
+    it('should split funds to registered partners', function () {
         var splitter;
 
         var partner_one = "0x905efee6442b06dc78af204f7df0afbfa52cede6";
@@ -53,19 +54,18 @@ contract('Splitter', function (accounts) {
         }).then(function () {
             splitter.partnerAdd(partner_two, 1);
         }).then(function () {
-            return splitter.partnersExists.call(partner_one);
+            return splitter.partnerExists.call(partner_one);
         }).then(function (result) {
             assert.isTrue(result);
-            return splitter.partnersExists.call(partner_one);            
+            return splitter.partnerExists.call(partner_one);
         }).then(function (result) {
             assert.isTrue(result);
-        })
-        .then(function () {
-            return splitter.split({value: sendValue});
+        }).then(function () {
+            return splitter.split({ value: sendValue });
         }).then(function (result) {
             assert.equal(
                 web3.eth.getBalance(partner_one).toNumber(),
-                partner_one_starting_balance.add(sendValue.div(2)).toNumber(), 
+                partner_one_starting_balance.add(sendValue.div(2)).toNumber(),
                 "Partner one should increase its balance by 50 wei"
             )
 
@@ -73,7 +73,48 @@ contract('Splitter', function (accounts) {
                 web3.eth.getBalance(partner_two).toNumber(),
                 partner_two_starting_balance.add(sendValue.div(2)).toNumber(),
                 "Partner two should increase its balance by 50 wei"
-            )            
+            )
         });
     })
 });
+
+contract('Splitter', function (accounts) {
+    it('should allow only owner or partners to make changes', function () {
+        var splitter;
+
+        owner = accounts[0];
+        partner_one = accounts[1];
+        partner_two = accounts[2];
+
+        Splitter.new({ from: owner }).then(function (instance) {
+            splitter = instance;
+            return splitter.partnerAdd(partner_two, 1, { from: partner_one })
+        }).then(function (result) {
+            assert.equal(0, result.receipt.status, "Adding a partner by a non partner / non owner should fail");
+            return splitter.partnerExists(partner_two);
+        }).then(function( result) {
+            assert.isTrue(!result, "Should not have been able to add a partner");
+        });
+    });
+
+    it('should allow partners to add more partners', function () {
+        var splitter;
+
+        var owner = accounts[0];
+        var partner_one = accounts[1];
+        var partner_two = accounts[2];
+
+        Splitter.new({ from: owner }).then(function (instance) {
+            splitter = instance;
+            return splitter.partnerAdd(partner_one, 1, { from: owner });
+        }).then(function (result) {
+            assert.equal(1, result.receipt.status, "Owner should be able allowed to add partners");
+            return splitter.partnerAdd(partner_two, 1, { from: owner });
+        }).then(function (result) {
+            assert.equal(1, result.receipt.status, "Partner should be able allowed to add more partners");
+            return splitter.partnerExists(partner_two);
+        }).then(function (result) {
+            assert.isTrue(result, "Partner one should be able to add more partners");
+        })
+    })
+})
