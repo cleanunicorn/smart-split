7ad606318f542b65175ed6f26dab39b1ba9358df
Daniel Luca <daniel.luca@consensys.net>
2017-11-07 10:00:54 +0200

    Rewrite tests with async await pattern

--- test/splitter.js
+++ test/splitter.js
@@ -10,9 +10,8 @@ contract('Splitter', function (accounts) {
     const somebody = accounts[8];
     const payer = accounts[9];
 
-    it('should not error', function (done) {
+    it('should not error', async function () {
         assert.isTrue(true);
-        done();
     });
 
     it('should save partner with correct weight', async function () {
@@ -28,78 +27,69 @@ contract('Splitter', function (accounts) {
         assert.isTrue(weightRes.equals(100), 'Weight should match the saved weight for a partner');
     });
 
-    it('should split funds to registered partners', function (done) {
-        var splitter;
+    it('should split funds to registered partners', async function () {
+        const sendValue = new BigNumber(web3.toWei(100, "wei"))
+        
+        const partner_one_starting_balance = web3.eth.getBalance(partner_one);
+        const partner_two_starting_balance = web3.eth.getBalance(partner_two);
+        
+        const splitter = await Splitter.new({ from: owner });
+        
+        const partner_one_add_result = await splitter.partnerAdd(partner_one, 1, { from: owner });
+        const partner_two_add_result = await splitter.partnerAdd(partner_two, 1, { from: owner });
 
-        var sendValue = new BigNumber(web3.toWei(100, "wei"))
+        assert.isTrue(
+            await splitter.partnerExists.call(partner_one),
+            "Owner should be able to add partner one"
+        );
 
-        var partner_one_starting_balance = web3.eth.getBalance(partner_one);
-        var partner_two_starting_balance = web3.eth.getBalance(partner_two);
+        assert.isTrue(
+            await splitter.partnerExists.call(partner_two),
+            "Owner should be able to add partner one"
+        );
 
-        Splitter.new({ from: owner }).then(function (instance) {
-            splitter = instance;
-            splitter.partnerAdd(partner_one, 1, { from: owner });
-        }).then(function () {
-            splitter.partnerAdd(partner_two, 1, { from: owner });
-        }).then(function () {
-            return splitter.partnerExists.call(partner_one);
-        }).then(function (result) {
-            assert.isTrue(result, "Owner should be able to add partner");
-            return splitter.partnerExists.call(partner_two);
-        }).then(function (result) {
-            assert.isTrue(result);
-        }).then(function () {
-            return splitter.send(sendValue, { from: payer });
-        }).then(function (result) {
-            assert.isTrue(result.receipt.gasUsed < 90000, "Split() should use less than 90000 gas")
+        const tx = await splitter.send(sendValue, {from: payer});
 
-            assert.equal(
-                web3.eth.getBalance(partner_one).toNumber(),
-                partner_one_starting_balance.add(sendValue.div(2)).toNumber(),
-                "Partner one should receive 50 wei"
-            )
+        assert.isTrue(
+            tx.receipt.gasUsed < 90000,
+            "Split() should use less than 90000 gas"
+        );
 
-            assert.equal(
-                web3.eth.getBalance(partner_two).toNumber(),
-                partner_two_starting_balance.add(sendValue.div(2)).toNumber(),
-                "Partner two should receive 50 wei"
-            )
+        assert.equal(
+            web3.eth.getBalance(partner_one).toNumber(),
+            partner_one_starting_balance.add(sendValue.div(2)).toNumber(),
+            "Partner one should receive 50 wei"
+        )
 
-            done();
-        });
+        assert.equal(
+            web3.eth.getBalance(partner_two).toNumber(),
+            partner_two_starting_balance.add(sendValue.div(2)).toNumber(),
+            "Partner two should receive 50 wei"
+        )
     });
 
-    it('should allow only owner or partners to make changes', function (done) {
-        var splitter;
+    it('should allow only owner or partners to make changes', async function () {
+        const splitter = await Splitter.new({ from: owner })
 
-        Splitter.new({ from: owner }).then(function (instance) {
-            splitter = instance;
-            return splitter.partnerAdd(partner_one, 1, { from: somebody })
-        }).then(function (result) {
+        try {
+            const partner_add_result = await splitter.partnerAdd(partner_one, 1, { from: somebody })
             assert.fail("Trying to add a partner by somebody unauthorized should fail");
-        }).catch(function (result) {
-            return splitter.partnerExists(partner_one);
-        }).then(function (result) {
-            assert.isTrue(!result, "Should not have been able to add a partner");
-            done();
-        });
+        } catch (e) {
+            const partner_one_exists = await splitter.partnerExists(partner_one);
+            assert.isTrue(!partner_one_exists, "Should not have been able to add a partner");
+        }
     });
 
-    it('should allow partners to add more partners', function (done) {
-        var splitter;
+    it('should allow partners to add more partners', async function () {
+        const splitter = await Splitter.new({ from: owner });
 
-        Splitter.new({ from: owner }).then(function (instance) {
-            splitter = instance;
-            return splitter.partnerAdd(partner_one, 1, { from: owner });
-        }).then(function (result) {
-            assert.equal(1, result.receipt.status, "Owner should be able allowed to add partners");
-            return splitter.partnerAdd(partner_two, 1, { from: owner });
-        }).then(function (result) {
-            assert.equal(1, result.receipt.status, "Partner should be able allowed to add more partners");
-            return splitter.partnerExists(partner_two);
-        }).then(function (result) {
-            assert.isTrue(result, "Partner one should be able to add more partners");
-            done();
-        })
+        const partner_one_added = await splitter.partnerAdd(partner_one, 1, { from: owner });
+        assert.equal(1, partner_one_added.receipt.status, "Owner should be able allowed to add partners");
+            
+        const partner_two_added = await splitter.partnerAdd(partner_two, 1, { from: partner_one });
+        assert.equal(1, partner_two_added.receipt.status, "Owner should be able allowed to add partners");
+
+        const partner_two_exists = await splitter.partnerExists(partner_two);
+        assert.isTrue(partner_two_exists, "Partner one should be able to add more partners");
     });
 });
